766c3e5fffd209b1e05c0dccae81652478aa1db2
shivam-1102 <shivamrahinj11@gmail.com>
2024-01-23 10:40:43 +0530

    First Commit

---
+++ addition.sh
@@ -0,0 +1,6 @@
+read -p "Enter 1st Number": x
+read -p" Enter 2nd nUmber": 
+echo $x
+echo $y
+Z=$(( $x + $y ))
+echo $Z
---
+++ ifstatement.sh
@@ -0,0 +1,6 @@
+read -p"Enter the nu:" a
+read -p"Enter the nu:" b
+if [ $a -gt $b ]
+then 
+echo $a is greater than $b
+fi
